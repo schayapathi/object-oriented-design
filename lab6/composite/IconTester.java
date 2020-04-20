2c5752f89cfdad4a9d5a9f7e9242ac404f5cf463
Swati Chayapathi <12708259+schayapathi@users.noreply.github.com>
2020-04-19 21:10:46 -0700

    composite

---
+++ lab6/composite/IconTester.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class IconTester
+{
+   public static void main(String[] args)
+   {
+      CompositeIcon compositeIcon = new CompositeIcon();
+      compositeIcon.addIcon(new MarsIcon(50));
+      compositeIcon.addIcon(new ImageIcon("globe.gif"));
+      JOptionPane.showMessageDialog(
+            null, 
+            "Hello, Composite!",
+            "Message",
+            JOptionPane.INFORMATION_MESSAGE,
+            compositeIcon);
+      System.exit(0);
+   }
+}
