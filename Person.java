3ce865501ded83f2524ddd241c581a56b4fe833a
Swati Chayapathi <12708259+schayapathi@users.noreply.github.com>
2020-05-10 17:17:20 -0700

    hw3

---
+++ Person.java
@@ -0,0 +1,22 @@
+
+public class Person {
+	private String name;
+	private int id;
+	
+	public Person(String name, int id)
+	{
+		this.name = name;
+		this.id = id;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+}
